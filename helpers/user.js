c1a04a81a9b2b2db6c324cb3ea93e29be71caa62
Thinh Le <lqthinh93@gmail.com>
2019-05-14 17:54:14 +0700

    create user's helper to validate authenticated user

---
+++ helpers/user.js
@@ -0,0 +1,19 @@
+const jwt = require("jsonwebtoken");
+const config = require("../config");
+
+module.exports = {
+  validateUser: function(req, res, next) {
+    jwt.verify(req.headers["authorization"], config.JWT_SECRET, function(
+      err,
+      decoded
+    ) {
+      if (err) {
+        res.json({ status: "error", message: err.message, value: null });
+      } else {
+        // add user id to request
+        req.body.userId = decoded.id;
+        next();
+      }
+    });
+  }
+};
